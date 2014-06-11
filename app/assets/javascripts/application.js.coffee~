9b6e2f0f857ad60a51eb26681634d1ef60f3df71
nyros <nyros@nyros.(none)>
2014-06-11 12:12:58 +0530

    initial commit

---
+++ app/assets/javascripts/application.js.coffee~
@@ -0,0 +1,18 @@
+//= require jquery
+//= require bootstrap
+#// require bootstrap-transition
+#// require bootstrap-alert
+#// require bootstrap-button
+#// require bootstrap-carousel
+#// require bootstrap-collapse
+#// require bootstrap-dropdown
+#// require bootstrap-modal
+#// require bootstrap-scrollspy
+#// require bootstrap-tab
+#// require bootstrap-tooltip
+#// require bootstrap-popover
+#// require bootstrap-typeahead
+#// require bootstrap-colorpicker
+//= require_tree .
+
+  
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapDemo::Application.config.secret_token = 'ca635c2a0619c542c001471abe33c5095e12820a1fbf6c68e18a5d231d4413f1f844e95a8589f82b5a6eefc57e79d8a5747ec9b7d95a5976b17925f8835a5bff'
